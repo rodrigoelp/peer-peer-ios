54c25b4837d391b01a68407c033b81eb1ba11300
Rodrigo Landaeta <rlandaeta@cochlear.com>
2019-05-22 11:38:07 +1000

    defined the view controller as a delegate of a peer session

--- src/peerMessages/ViewController.swift
+++ src/peerMessages/ViewController.swift
@@ -1,18 +1,23 @@
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MCSessionDelegate {
     @IBOutlet weak var startHostButton: UIButton!
     @IBOutlet weak var joinHostButton: UIButton!
     @IBOutlet weak var messageText: UITextField!
     @IBOutlet weak var sendMessageButton: UIButton!
     @IBOutlet weak var messagesLog: UITextView!
     
+    var peerId: MCPeerID!
+    var session: MCSession!
+    var advertiser: MCAdvertiserAssistant!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        messagesLog.text = ""
-        messageText.isEnabled = false
-        sendMessageButton.isEnabled = false
+        setInitialUIState()
+        
+        prepareSession()
     }
     
     @IBAction func startHost(_ sender: Any) {
@@ -22,5 +27,34 @@ class ViewController: UIViewController {
     }
     
     @IBAction func sendMessage(_ sender: Any) {
+    }
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+    }
+    
+    private func setInitialUIState() {
+        messagesLog.text = ""
+        messageText.isEnabled = false
+        sendMessageButton.isEnabled = false
+    }
+    
+    private func prepareSession() {
+        // let's create our identity in the peer network.
+        peerId = MCPeerID(displayName: "📨 \(UIDevice.current.name)")
+        // then we prepare a session to be used and listen to events.
+        session = MCSession(peer: peerId, securityIdentity: .none, encryptionPreference: .required)
+        session.delegate = self
     }
 }
